5994e3ebabd75a73d2caa0a1b4242eb604ea86f0
DevOps <borysenkoif@gmail.com>
2023-08-21 21:00:53 +0300

    change file README

--- Hometask06_Linux2/command.sh
+++ Hometask06_Linux2/command.sh
@@ -1,5 +1,17 @@
-mkdir -p ~/linux_p2 && cd ~/linux_p2 && touch {1..20}.txt && dd if=/dev/urandom of={1..20}.txt bs=1024 count=1
+#!/bin/bash
+
+#версія 1
+
+#mkdir -p ~/linux_p2 && cd ~/linux_p2 && touch {1..20}.txt && dd if=/dev/urandom of={1..20}.txt bs=1024 count=1
 # ~/linux_p2 створює дерикторію де умова mkdir -p ~/linux_p2 якщо вона створена
 # далі переходить у створену дерикторію cd ~/linux_p2
 # Створює 20 файлів .txt командою =.>   touch {1..20}.txt
 # Запис рандомних даних у файли Командою  =>    dd if=/dev/urandom of={1..20}.txt bs=1024 count=1
+
+#версія 2
+#mkdir -p ~/linux_p2 && cd ~/linux_p2 && touch {1..20}.txt && for i in {1..20}; do dd if=/dev/urandom of="$i.txt" bs=1024 count=1; done && echo 'done' > command.sh && chmod +x command.sh
+
+#for i in {1..20}; do touch ${i}.txt;
+
+
+mkdir -p ~/linux_p2 && cd ~/linux_p2 && for i in {1..20}; do touch ${i}.txt && dd if=/dev/urandom of="${i}.txt" bs=1024 count=1; done && echo 'done' > command.sh && chmod +x command.sh
--- README.md
+++ README.md
@@ -8,8 +8,13 @@
 
 | Task list | Date |
 | ----- | ----- |
-| Task 1 | *Scrum* and **DevOps** |
-| Task 2 | Git and Github |
+| Task 1 | SCM. Git |
+| Task 2 | Networking fundamentals |
+| Task 3 | Networking part 2|
+| Task 4 |Virtual Machines, Vagrant, Cloud Providers |
+| Task 5 | Linux commands, part 1|
+| Task 6 |  Linux commands part 2 |
+
 
 ## Summary:
 - add Oleh as reviewer
